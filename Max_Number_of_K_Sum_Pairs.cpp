19ac916d2268a52e2bb6f7604e894a5b0cbab9dd
Priya_Pramanick <priyapramanick17@gmail.com>
2022-05-04 22:50:01 +0530

    4th may,2022 daily challenge

---
+++ Max_Number_of_K_Sum_Pairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+ map<int,int>m;
+        int c=0;
+for(auto i:nums){
+    if(m[k-i]){
+    m[k-i]--;
+        m[i]--;
+        c++;
+    }
+    m[i]++;
+}
+        return c;
+    }
+};
